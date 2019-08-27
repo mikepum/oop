ac4a32adb225212dd020d408965f28be51905986
Mike <mikepumford@gmail.com>
2019-08-27 18:10:34 +0100

    first commit

---
+++ oop1.js
@@ -0,0 +1,30 @@
+// oop example
+
+class Car {
+    constructor(name, engine) {
+        this._name = name;
+        this._engine = engine;
+        this._mileage = 0;
+    }
+    get name() {
+        return this._name;
+    }
+    get engine() {
+        return this._engine;
+    }
+    get mileage() {
+        return this._mileage;
+    }
+    increaseMileage(){
+        this._mileage += 10000;
+    }
+} 
+
+const rs4 = new Car("RS4", "4.2");
+const honda = new Car("Accord","2.0");
+console.log(rs4.name + " " + rs4.engine);
+console.log(rs4.mileage);
+// rs4.increaseMileage();
+rs4.increaseMileage();
+console.log(rs4.mileage);
+console.log(honda.name + " " + honda.engine);
